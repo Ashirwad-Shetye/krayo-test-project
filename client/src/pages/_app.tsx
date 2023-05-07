b9770ac03c3ecfab372ea3741cfa2a310a6f4027
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-05-08 02:14:54 +0530

    chore: dark theme for clerk client

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { dark } from "@clerk/themes";
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+      {...pageProps}
+    >
       <Component {...pageProps} />
     </ClerkProvider>
   );
